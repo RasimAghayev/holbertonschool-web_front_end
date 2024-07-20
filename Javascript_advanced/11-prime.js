49b0e41eba65b43242f5bf883ce6cdca0ad8c03d
Rasim Aghayev <rasimaqayev@gmail.com>
2024-07-20 11:55:36 +0400

    11. Changing stack order using setTimeout

---
+++ Javascript_advanced/11-prime.js
@@ -0,0 +1,39 @@
+function countPrimeNumbers() {
+  function isPrime(num) {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+    if (num % 2 === 0 || num % 3 === 0) return false;
+    for (let i = 5; i * i <= num; i += 6) {
+      if (num % i === 0 || num % (i + 2) === 0) return false;
+    }
+    return true;
+  }
+
+  let count = 0;
+  for (let i = 2; i <= 100; i++) {
+    if (isPrime(i)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log("Start timing...");
+
+setTimeout(() => {
+  const start = performance.now();
+
+  for (let i = 0; i < 100; i++) {
+    countPrimeNumbers();
+  }
+
+  const end = performance.now();
+
+  console.log(
+    `Execution time of calculating prime numbers 100 times was ${
+      end - start
+    } milliseconds.`
+  );
+}, 0);
+
+console.log("End timing...");

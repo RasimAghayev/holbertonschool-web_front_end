f99386664d07572b8382d0f5cd04c592eb002a9c
Rasim Aghayev <9415@holbertonstudents.com>
2024-08-10 07:19:08 +0000

    3. Delete cookie and mini application

--- Cookies_local_storage/webpack.config.js
+++ Cookies_local_storage/webpack.config.js
@@ -1,5 +1,13 @@
+const path = require("path");
+
 module.exports = {
-    mode: 'development', // or 'production' or 'none'
-    // other configurations...
-  };
-  
+  mode: "development", // or 'production' or 'none'
+  //...
+  devServer: {
+    allowedHosts: 'all',
+    static: {
+      directory: path.join(__dirname, ""),
+    },
+    compress: true,
+  },
+};
